c5cb30f234f136c8e346b79aaf2c975035f1dd6d
lcfevr@163.com <lcfevr6509>
2017-06-19 15:13:56 +0800

    update zip

--- zip.js
+++ zip.js
@@ -2,7 +2,7 @@
  * Created by admin on 2017/6/19.
  */
 var info = require('./package.json');
-var zip = info.name + "_" + info.version;
+var zip = info.name + "_" + info.version + "_" + new Date().toDateString() ;
 
 var zipper = require('zip-local');
 
